00881beb8c007667529e6bacb49e9e76b7235d4b
Yun-Fan Chen <o07080826mina@gmail.com>
2021-10-30 14:18:40 +0800

    建立子樣板

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Home</title>
+</head>
+    <body>
+        @extends('layouts.master')
+
+        @section('title','Home')
+
+        @section('content')
+
+        <h1>Home</h1>
+        <p>This is my body content.</p>
+
+        @endsection
+    </body>
+</html>
