aa71eb5039393ab22e4c38403c199257189bc023
Yun-Fan Chen <o07080826mina@gmail.com>
2021-10-30 14:06:48 +0800

    HomeController建立&Route修改

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('home', function (){
+Route::get('home',[\App\Http\Controllers\HomeController::class, 'index'])->name('home.index');
+
+/*Route::get('home', function (){
     return view('home.index');
-});
+});*/
